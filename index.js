cf97779d9481c1e65bb17bcd6ab1e5cbcefc830c
github username <newmanbradm@gmail.com>
2018-12-29 11:08:45 -0500

    add notes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* Your code goes here */
+// global variable can be changed throughout code
 var customerName = 'bob'
 
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase()
 }
+
+//avoid defining global variables
 
 var bestCustomer
 
@@ -15,11 +18,15 @@ function overWriteBestCustomer() {
     bestCustomer = 'maybe bob'
 }
 
+//cannot reassign const variable
+
 const leastFavoriteCustomer = 'someone'
 
 function changeLeastFavoriteCustomer() {
     leastFavoriteCustomer = 'someone else'
 }
+
+//cannot double assign with let and const keywords
 
 function attemptTwoFavoriteCustomers() {
     let favoriteCustomer = 'brad'
